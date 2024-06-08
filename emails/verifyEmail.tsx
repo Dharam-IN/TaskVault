952637ab8a8d9a5a1b178cb7d05e9c3a6f5b8b85
dharam-in <dharamdotin@gmail.com>
2024-06-08 17:09:43 +0530

    Email Verify Functionality Added

--- emails/verifyEmail.tsx
+++ emails/verifyEmail.tsx
@@ -1,31 +1,54 @@
-import * as React from 'react';
-import { Html } from '@react-email/html';
+import {
+  Html,
+  Head,
+  Font,
+  Preview,
+  Heading,
+  Row,
+  Section,
+  Text,
+} from '@react-email/components';
 
-interface EmailTemplateProps {
+interface VerificationEmailProps {
   username: string;
   otp: string;
 }
 
-const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  username,
-  otp,
-}) => (
-  <Html lang="en">
-    <div style={{ fontFamily: 'Arial, sans-serif', lineHeight: '1.6' }}>
-      <div style={{ padding: '20px', border: '1px solid #eaeaea', borderRadius: '5px', maxWidth: '600px', margin: 'auto' }}>
-        <h1 style={{ color: '#333', textAlign: 'center' }}>Welcome to TodoVault, {username}!</h1>
-        <p>Hi {username},</p>
-        <p>
-          Thank you for signing up for <strong>TodoVault</strong>! We are excited to have you on board.
-        </p>
-        <p>
-          TodoVault is your ultimate task management solution, designed to help you organize your tasks efficiently and
-          boost your productivity. Whether you're managing personal to-dos or professional projects, TodoVault has got you covered.
-        </p>
-        <p>
-          To get started, please verify your email address using the OTP below:
-        </p>
-        <div style={{ textAlign: 'center', margin: '20px 0' }}>
+export default function VerificationEmail({ username, otp }: VerificationEmailProps) {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>Verification Code</title>
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: 'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+            format: 'woff2',
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      <Preview>Here&apos;s your verification code: {otp}</Preview>
+      <Section style={{ fontFamily: 'Roboto, Verdana, sans-serif', lineHeight: '1.6', padding: '20px', border: '1px solid #eaeaea', borderRadius: '5px', maxWidth: '600px', margin: 'auto' }}>
+        <Row>
+          <Heading as="h2" style={{ color: '#333', textAlign: 'center' }}>Hello {username},</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for signing up for <strong>TodoVault</strong>! We are excited to have you on board.
+          </Text>
+        </Row>
+        <Row>
+          <Text>
+            TodoVault is your ultimate task management solution, designed to help you organize your tasks efficiently and boost your productivity. Whether you're managing personal to-dos or professional projects, TodoVault has got you covered.
+          </Text>
+        </Row>
+        <Row>
+          <Text>To get started, please verify your email address using the OTP below:</Text>
+        </Row>
+        <Row style={{ textAlign: 'center', margin: '20px 0' }}>
           <div
             style={{
               backgroundColor: '#007bff',
@@ -39,18 +62,18 @@ const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
           >
             {otp}
           </div>
-        </div>
-        <p>
-          If you did not sign up for TodoVault, please ignore this email.
-        </p>
-        <p>
-          Best regards,<br />
-          The TodoVault Team <br />
-          GitHub: <a href="https://github.com/Dharam-IN">Dharam-IN</a>
-        </p>
-      </div>
-    </div>
-  </Html>
-);
-
-export default EmailTemplate;
+        </Row>
+        <Row>
+          <Text>If you did not sign up for TodoVault, please ignore this email.</Text>
+        </Row>
+        <Row>
+          <Text>
+            Best regards,<br />
+            The TodoVault Team <br />
+            GitHub: <a href="https://github.com/Dharam-IN" target='_blank'>Dharam-IN</a>
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+}
--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,1 +1,23 @@
 import { resend } from "@/lib/resend";
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/verifyEmail";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse>{
+    try {
+
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'TodoVault | Verification Code!',
+            react: VerificationEmail({username, otp: verifyCode}),
+        });
+
+        return {success: true, message: "Verification Code Send Successfully!"}
+    } catch (error) {
+        return {success: false, message: "Error Send Verification Code"}
+    }
+}
